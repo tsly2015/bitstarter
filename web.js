98e5f0dddb109ecc3f321378225fda077ebad4c9
tsly2015 <zhenw.wang@gmail.com>
2013-07-11 18:47:25 -0400

    html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var fs = require('fs');
+  var buffer = fs.readFileSync("index.html");
+  var outputstring = buffer.toString()
+  response.send(outputstring);
 });
 
 var port = process.env.PORT || 5000;
